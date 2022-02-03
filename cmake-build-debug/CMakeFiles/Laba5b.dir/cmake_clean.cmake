7d91d94385c19936a4656cba942d707d2cd0a340
Vuulgrim <cool.Boris2002@mail.ru>
2022-02-03 18:17:09 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Laba5b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laba5b.dir/main.c.obj"
+  "Laba5b.exe"
+  "Laba5b.exe.manifest"
+  "Laba5b.pdb"
+  "libLaba5b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Laba5b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Laba5b.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Laba5b.
+# This may be replaced when dependencies are built.
